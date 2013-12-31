d168e94e858c1f4ebb07aa0aa9848962b0be138a
uzuky <uzuky04@gmail.com>
2014-01-01 02:21:10 +0900

    Update imgMiiverse.js

--- imgMiiverse.js
+++ imgMiiverse.js
@@ -6,11 +6,11 @@ javascript: (function () {
 		var s = D.getElementsByClassName('screenshot-container');
 			//p = D.getElementsByClassName('post-content-memo'), tegakitoukounoyatsu
 			if (s.length == 0) {
-				alert('スクリーンショットがないよ！\n\n連絡先とか\nhttp://let.hatelabo.jp/uzuky/let/hJmc4ICygZk_');
+				alert('スクリーンショットがないよ！\n\n連絡先とか\nhttps://github.com/uzuky/imgMiiverse');
 			} else {
 				var u = s[0].innerHTML.replace(/.+?"(.+?)".+/, '$1'),
 				m = D.createElement('div');
-				m.innerHTML = '<input type="button" value="スクリーンショットだけ表示" onClick="window.open(\'' + u + '\',\'_blank\',\'width=800,height=450\');" style="font-size:12px;"><br><b>スクショURL</b><input type="text" value="' + u + '" readonly="true" onclick="this.select();" style="font-size:12px;"><br><small><b>※スクリーンショットはjpegです<br>※3DSのスクリーンショットは<br>開発者による投稿の一部を除いてmpoです</b><br>※ちなみに手書き投稿の画像はpngです<br><a href="http://let.hatelabo.jp/uzuky/let/hJmc4ICygZk_" target="_blank" title="version">' + V + '</a></small>';
+				m.innerHTML = '<input type="button" value="スクリーンショットだけ表示" onClick="window.open(\'' + u + '\',\'_blank\',\'width=800,height=450\');" style="font-size:12px;"><br><b>スクショURL</b><input type="text" value="' + u + '" readonly="true" onclick="this.select();" style="font-size:12px;"><br><small><b>※スクリーンショットはjpegです<br>※3DSのスクリーンショットは<br>開発者による投稿の一部を除いてmpoです</b><br>※ちなみに手書き投稿の画像はpngです<br><a href="https://github.com/uzuky/imgMiiverse" target="_blank" title="version">' + V + '</a></small>';
 				m.style.textAlign = 'center';
 				m.style.padding = '10px';
 				m.style.position = 'fixed';
@@ -23,7 +23,7 @@ javascript: (function () {
 				document.body.appendChild(m);
 			}
 	} else if (H.match(/s:\/\/miiverse\.nintendo\.net\//)) {
-		alert('スクリーンショット付きpostの個別ページで使えます\n\n連絡先とか\nhttp://let.hatelabo.jp/uzuky/let/hJmc4ICygZk_');
+		alert('スクリーンショット付きpostの個別ページで使えます\n\n連絡先とか\nhttps://github.com/uzuky/imgMiiverse');
 	} else {
 		var t = confirm('Miiverseのスクリーンショット付きpostの個別ページで使えます。\nhttps://miiverse.nintendo.net/\nに移動しますか？');
 		if (t == true) {
